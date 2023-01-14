7d16641603d0dca3fa764f807ca81713374669cb
fabian57fabian <fabian_57@yahoo.it>
2023-01-14 12:13:19 +0100

    Added width and height args

--- create_dataset.py
+++ create_dataset.py
@@ -3,6 +3,8 @@ from src.simulator import evolve
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument("-W", "--width", type=int, default=1280, help="Dataset frame height (e.g. 1280)")
+    parser.add_argument("-H", "--height", type=int, default=720, help="Dataset frame width (e.g. 720)")
     args = parser.parse_args()
 
     evolve()
