2ca74a0e5b799e5870cb002aa6cd8a2289ecbc43
AngeloDamante <angelo.damante16@gmail.com>
2023-01-16 19:42:01 +0100

    test: setting up environment

--- src/evolver.py
+++ src/evolver.py
@@ -1,6 +1,10 @@
 import numpy as np
 import os
+import sys
+__location__ = os.path.realpath(os.path.join(
+    os.getcwd(), os.path.dirname(__file__)))
+sys.path.insert(0, __location__)
 
 
-def evolve(fame_w:int, frame_h:int, origin_w:int, origin_h:int, patch:np.ndarray, route:list, fps:int) -> list:
+def evolve(fame_w: int, frame_h: int, origin_w: int, origin_h: int, patch: np.ndarray, route: list, fps: int) -> list:
     pass
---
+++ tests/test_evolver.py
@@ -0,0 +1,10 @@
+import unittest
+import src.evolver as evolver
+
+class Test_evolver(unittest.TestCase):
+    def test_init(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
