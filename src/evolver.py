c25a59d5d98c27e1c2dcddcd2fd250d9d19b4002
AngeloDamante <angelo.damante16@gmail.com>
2023-01-19 16:10:59 +0100

    refactor: turn evolve function to class

--- src/evolver.py
+++ src/evolver.py
@@ -1,62 +1,108 @@
+import sys
+import os
+import logging
+import cv2
 import numpy as np
 from typing import Tuple
-import cv2
-import logging
-import os
-import sys
-__location__ = os.path.realpath(os.path.join(
-    os.getcwd(), os.path.dirname(__file__)))
+from MovementType import MovementType
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 sys.path.insert(0, __location__)
 
-from MovementType import MovementType
 
+class Evolver:
+    """ Evolver to implement custom video with patch applied.
 
-def evolve(frame_w: int, frame_h: int, origin_w: int, origin_h: int, patch: np.ndarray, route: list, fps: int) -> Tuple[list, list]:
-    # q_i, q_f, t_f [s]
-    origin = np.array([origin_h, origin_w], dtype=float)
-    dest = np.array([route[0][0], route[0][1]], dtype=float)
-    t_f = route[0][3] / 1000
-    
-    # compute num frames and step
-    num_frames = int(fps * t_f)
-    step = float(1 / fps)
-    
-    # extract commands    
-    commands = [route[i][2] for i in range(len(route))]
+        Attributes:
+            frame_w(int) 
+            frame_h(int)
+            origin_w(int): origin along y-axis
+            origin_h(int): origin along x-axis
+            patch(np.ndarray): to apply to the frame
+            fps(int)
+    """
 
-    frames = []
-    for command in commands:
-        if command == MovementType.linear:
+    def __init__(self,
+                 frame_w: int,
+                 frame_h: int,
+                 origin_w: int,
+                 origin_h: int,
+                 patch: np.ndarray,
+                 fps: int) -> None:
+
+        # input
+        self.frame_w = frame_w
+        self.frame_h = frame_h
+        self.origin = np.array([origin_h, origin_w], dtype=float)
+        self.patch = patch
+        self.fps = fps
+
+        # internal states
+        self.v = 0
+        self.step = float(1 / self.fps)
+
+        # output
+        self.frames = []
+        self.gth = []
+
+    def compute_evolutions(self, route: list) -> Tuple[list, list]:
+        """ Compute evolutions for each steps in route list.
+
+            Args:
+                route(list): list of steps for patch applied
+
+            Returns:
+                frames(list): list of frames to animate the path
+                gth(list): list of path center coord in the frame
+        """
+        for (d_w, d_h, command, time_ms) in route:
+
+            # compute dest and final time [s]
+            dest = np.array([d_h, d_w], dtype=float)
+            t_f = time_ms / 1000
+
             # compute velocity
-            v = (dest - origin) / t_f
+            num_frames = int(self.fps * t_f)
+            self.v = (dest - self.origin) / t_f
 
-            frames = []
-            for i in range(num_frames + 1):
-                frame = np.zeros((frame_h, frame_w))
-                t = i * step
+            if command == "linear":
+                self._compute_linear(num_frames)
 
-                # motion law
-                x = origin + v*t
+            elif command == "acc":
+                # TODO
+                pass
 
-                # apply patch
-                r_i = int(x[0] - patch.shape[0] / 2)
-                r_f = int(x[0] + patch.shape[0] / 2)
-                c_i = int(x[1] - patch.shape[1] / 2)
-                c_f = int(x[1] + patch.shape[1] / 2)
-                
-                frame[r_i:r_f, c_i:c_f] = patch
-                frames.append(frame)
-                
-        elif command == MovementType.acc:
-            pass
-        elif command == MovementType.dec:
-            pass
-        elif command == MovementType.trap:
-            pass
-        else:
-            logging.error("Wrong type!")
-            pass
-    
+            elif command == "dec":
+                # TODO
+                pass
 
-def compute_linear(origin: np.ndarray, dest: np.ndarray, t_f:float, num_frames:int, step:float) -> list:
-    pass
+            elif command == MovementType.trap:
+                # TODO
+                pass
+            else:
+                logging.error("Wrong type!")
+                pass
+
+            # update origin for next command
+            self.origin = dest
+
+        return self.frames, self.gth
+
+    def _compute_linear(self, num_frames: int) -> None:
+        """ Compute frames with uniformly rectilinear motion for patch.
+        
+            Args:
+                num_frames(int): number of desired frames 
+        """
+        for i in range(num_frames + 1):
+            frame = np.zeros((self.frame_h, self.frame_w, 3))
+
+            # motion law
+            t = i * self.step
+            x = self.origin + self.v * t
+
+            frame = self.apply_patch(frame, self.patch, x)
+
+            # save
+            self.frames.append(frame)
+            self.gth.append(x)
