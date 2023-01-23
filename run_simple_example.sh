0b5f54bfeb679dfd3597c111d6e6541bcc6c568c
fabian57fabian <fabian_57@yahoo.it>
2023-01-23 19:01:02 +0100

    Added test sequences for fast execution

--- run_simple_example.sh
+++ run_simple_example.sh
@@ -2,6 +2,6 @@ python3 create_dataset.py \
 	--width=1280 \
 	--height=720 \
 	--background="0,0,0" \
-	--input-json="example_2_figures.json" \
-	--video="out_test.mp4" \
+	--input-json="test_seq_develop.json" \
+	--video="datasets_out/video_develop.mp4" \
 	--fps=30
