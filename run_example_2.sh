9bd2da36c5400340fe4e44183323518a3a1faa59
fabian57fabian <fabian_57@yahoo.it>
2023-01-23 23:24:26 +0100

    Added new background, route and sequence for test

---
+++ run_example_2.sh
@@ -0,0 +1,7 @@
+python3 create_dataset.py \
+	--width=640 \
+	--height=480 \
+	--background="maps_roma.jpg" \
+	--input-json="square_pentagon_map.json" \
+	--video="datasets_out/video_mcqueen_dev.mp4" \
+	--fps=30
