7e154306307c781ba954ace585223fbba5abf0dd
AngeloDamante <angelo.damante16@gmail.com>
2023-01-28 18:47:48 +0100

    feat: add args for create_random_routes.py script

--- create_random_routes.py
+++ create_random_routes.py
@@ -3,17 +3,26 @@ import random
 import os
 import argparse
 
-DIR_ROUTES = "routes"
-
-# TODO
 commands = ["const", "acc", "trap", "pause"]
 
 
-def routes_generator(num_instructions: int) -> list:
-    pass
+def routes_generator(num_instructions: int, duration: int, frame_size: tuple) -> list:
+    return []
 
 
 if __name__ == '__main__':
-    # TODO checks for argparse
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--name", required=True, type=str, help="filename of random routes")
+    parser.add_argument("-i", "--instructions", required=True, type=int, help="number of instructions")
+    parser.add_argument("-d", "--duration", type=int, default=100, help="video duration in [ms]")
+    parser.add_argument("-w", "--width", default=640, type=int, help="width of frame")
+    parser.add_argument("-h", "--height", default=480, type=int, help="height of frame")
 
+    # parsing
+    args = parser.parse_args()
+    instructions = routes_generator(args.instructions, args.duration, (args.width, args.height))
+
+    # save file
+    DIR_ROUTES = "routes"
+    with open(f"{DIR_ROUTES}/{args.name}.txt", 'w+') as outfile:
+        outfile.write(str(instructions))
