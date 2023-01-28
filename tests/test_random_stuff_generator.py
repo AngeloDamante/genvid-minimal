a814b97546c37e424c75f4e666640914c98046c3
AngeloDamante <angelo.damante16@gmail.com>
2023-01-28 20:34:04 +0100

    test: setting environment for random generator

---
+++ tests/test_random_stuff_generator.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class TestRandomRouteGenerator(unittest.TestCase):
+    def test_generate_number_with_sum(self):
+        # TODO
+        pass
+
+    def test_routes_generator(self):
+        # TODO
+        pass
+
+class TestRandomSequenceGenerator(unittest.TestCase):
+    def test_json_generator(self):
+        # TODO
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
