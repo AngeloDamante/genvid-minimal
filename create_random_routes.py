2be12f96f16879afe4b3a470e7494627d1071084
AngeloDamante <angelo.damante16@gmail.com>
2023-01-28 13:07:47 +0100

    wip: working for script to generate random routes

---
+++ create_random_routes.py
@@ -0,0 +1,19 @@
+import logging
+import random
+import os
+import argparse
+
+DIR_ROUTES = "routes"
+
+# TODO
+commands = ["const", "acc", "trap", "pause"]
+
+
+def routes_generator(num_instructions: int) -> list:
+    pass
+
+
+if __name__ == '__main__':
+    # TODO checks for argparse
+    pass
+
