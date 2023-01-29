711b940f8857571d37b9cbe21ac4b5861d7e1e6c
AngeloDamante <angelo.damante16@gmail.com>
2023-01-29 11:25:31 +0100

    refactor: remove logger

--- create_random_sequence.py
+++ create_random_sequence.py
@@ -1,5 +1,4 @@
 import json
-import logging
 import random
 import os
 import argparse
@@ -35,26 +34,9 @@ def json_generator(num_objects: int, routes: list = None, patches: list = None) 
     return seq
 
 
-def configure_logging(log_filename: str, log_console=False, log_lvl=logging.DEBUG):
-    """Cofnigure logging
-
-    Args:
-        log_filename:
-        log_console:
-        log_lvl:
-    """
-    if log_console:
-        logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
-                            level=log_lvl)
-    else:
-        logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
-                            filename=log_filename, level=log_lvl)
-
-
 if __name__ == '__main__':
     DIR_SEQUENCES = "sequences"
 
-    configure_logging("log_json_gen.log", True, log_lvl=logging.DEBUG)
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name", type=str, required=True, help="name of your random sequences")
     parser.add_argument("-o", "--number_objects", type=int, default=1, help="number of objects to be generated")
