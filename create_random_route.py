1bf9891ad34f6485381f3ab4b1511ed777f414fb
AngeloDamante <angelo.damante16@gmail.com>
2023-02-06 21:40:11 +0100

    fix: -H ambiguity with -h

--- create_random_route.py
+++ create_random_route.py
@@ -83,8 +83,8 @@ if __name__ == '__main__':
     parser.add_argument("-n", "--name", required=True, type=str, help="filename of random routes")
     parser.add_argument("-i", "--instructions", required=True, type=int, help="number of instructions")
     parser.add_argument("-d", "--duration", type=int, default=100, help="video duration in [ms]")
-    parser.add_argument("-w", "--width", default=640, type=int, help="width of frame")
-    parser.add_argument("-h", "--height", default=480, type=int, help="height of frame")
+    parser.add_argument("-W", "--width", default=640, type=int, help="width of frame")
+    parser.add_argument("-H", "--height", default=480, type=int, help="height of frame")
 
     # parsing
     args = parser.parse_args()
